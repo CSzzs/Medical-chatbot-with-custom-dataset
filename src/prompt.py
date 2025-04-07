3dd2a575c339739958f1d7fc5bfaafebd1dc0aa2
Chandima Senarathna <chandima.senarathna97@gmail.com>
2025-04-07 15:03:23 +0530

    first commit for MedicalBot_01 project

---
+++ src/prompt.py
@@ -0,0 +1,9 @@
+
+system_prompt = (
+    'You are an assitant for question answering tasks.'
+    'Use the following pieces of retrieved context to answer'
+    'The question if you do not know the answer say that you do not know the answer politely'
+    'use three sentence maximum and keep the answer clear and conscise'
+    '\n\n'
+    '{context}'
+)
