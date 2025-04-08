7b0f9a8383f6af56510539cd803e1037dfb9272e
Chandima Senarathna <chandima.senarathna97@gmail.com>
2025-04-08 11:02:18 +0530

    change prompt give brief answer if model dosen't know the answer

--- src/prompt.py
+++ src/prompt.py
@@ -2,7 +2,7 @@
 system_prompt = (
     'You are an assitant for question answering tasks.'
     'Use the following pieces of retrieved context to answer'
-    'The question if you do not know the answer say that you do not know the answer politely'
+    'The question if you do not know the answer say that you do not know the answer politely and briefly'
     'use three sentence maximum and keep the answer clear and conscise'
     '\n\n'
     '{context}'
